a1ba2ea3ce73c715ab34698b8e296fc1a29e093f
CrispyHarder <christian.harder96@gmail.com>
2021-10-21 11:18:36 +0200

    rewrote net_convergence_comparison

--- experiments/net_convergence_comparison.py
+++ experiments/net_convergence_comparison.py
@@ -6,47 +6,39 @@ import pandas
 import numpy as np
 import matplotlib.pyplot as plt 
 import yaml 
+from datetime import date
 
 logs_path = os.path.join('logs','exman-train-net.py','runs')
 runs = [os.path.join(logs_path,run) for run in os.listdir(logs_path) if run[:6] not in ['000001','000002']]
-x_files = []
-v_files = []
-h_files = []
-for run in runs:
-    file  = os.path.join(run,'train-net.py-logs.csv')
-    yaml_p = os.path.join(run,'params.yaml')
-    with open(yaml_p) as f:
-        dict = yaml.load(f)
-    if dict['mult_init_mode'] == 'xavier':
-        x_files.append(file)
-    if dict['mult_init_mode'] == 'vae':
-        v_files.append(file)
-    if dict['mult_init_mode'] == 'he':
-        h_files.append(file)
 
-x_data = np.genfromtxt(x_files[0],delimiter=',')[1:,1:6]
-for file in x_files[1:]:
-    x_data += np.genfromtxt(file,delimiter=',')[1:,1:6]
-x_data = x_data / len(x_files)
+INIT_NAMES = ['xavier','vae','he','vqvae1.0','vqvae1.2']
+METR_NAMES = ['loss','train_nll','test_nll','train_acc','test_acc']
+SAVE_PATH = os.path.join('..','..','small-results',str(date.today()),'init comparison')
+if not os.path.exists(SAVE_PATH):
+    os.makedirs(SAVE_PATH)
 
-v_data = np.genfromtxt(v_files[0],delimiter=',')[1:,1:6]
-for file in v_files[1:]:
-    v_data += np.genfromtxt(file,delimiter=',')[1:,1:6]
-v_data = v_data / len(v_files)
+data = []
 
-h_data = np.genfromtxt(h_files[0],delimiter=',')[1:,1:6]
-for file in h_files[1:]:
-    h_data += np.genfromtxt(file,delimiter=',')[1:,1:6]
-h_data = h_data / len(h_files)
-
-print(np.shape(v_data))
-metr_names = ['loss','train_nll','test_nll','train_acc','test_acc']
-inits = ['xavier','vae','he']
+for i,init in enumerate(INIT_NAMES):
+    files = []
     
-for i,m_name in enumerate(metr_names):
-    plt.plot(x_data[:,i],label='xavier')
-    plt.plot(v_data[:,i],label='vae')
-    plt.plot(h_data[:,i],label='he')
+    for run in runs:
+        file  = os.path.join(run,'train-net.py-logs.csv')
+        yaml_p = os.path.join(run,'params.yaml')
+        with open(yaml_p) as f:
+            dict = yaml.load(f)
+        if dict['mult_init_mode'] == init:
+            files.append(file)
+        
+    init_data = np.genfromtxt(files[0],delimiter=',')[1:,1:6]
+    for file in files[1:]:
+        init_data += np.genfromtxt(file,delimiter=',')[1:,1:6]
+    init_data = init_data / len(files)
+    data.append(init_data)
+
+for i,m_name in enumerate(METR_NAMES):
+    for j,init in enumerate(INIT_NAMES):
+        plt.plot(data[j][:,i],label=init)
     plt.title('{} convergence comparison'.format(m_name))
     plt.legend(loc='lower right')
     plt.savefig(os.path.join('..','..','small-results','14.10.2021','he_xav_vae init comparison',m_name))
