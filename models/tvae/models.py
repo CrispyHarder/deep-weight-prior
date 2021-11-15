edfd876d5e3225f57d2f2eefc3e7368765d22acd
CrispyHarder <christian.harder96@gmail.com>
2021-11-15 15:47:00 +0100

    changed activations from Relu to ELU and delted sigmoid in last layer

--- models/tvae/models.py
+++ models/tvae/models.py
@@ -4,10 +4,10 @@ def MLP_Encoder(s_dim, n_cin, n_hw):
     model = nn.Sequential(
                 nn.Conv2d(n_cin, s_dim*3,
                     kernel_size=n_hw, stride=1, padding=0),
-                nn.ReLU(True),
+                nn.ELU(True),
                 nn.Conv2d(s_dim*3, s_dim*2,
                     kernel_size=1, stride=1, padding=0),
-                nn.ReLU(True),
+                nn.ELU(True),
                 nn.Conv2d(s_dim*2, s_dim*2,
                     kernel_size=1, stride=1, padding=0))
     return model
@@ -16,11 +16,10 @@ def MLP_Decoder(s_dim, n_cout, n_hw):
     model = nn.Sequential(
                 nn.ConvTranspose2d(s_dim, s_dim*2, 
                     kernel_size=1, stride=1, padding=0),
-                nn.ReLU(True),
+                nn.ELU(True),
                 nn.ConvTranspose2d(s_dim*2, s_dim*3, 
                     kernel_size=1, stride=1, padding=0),
-                nn.ReLU(True),
+                nn.ELU(True),
                 nn.ConvTranspose2d(s_dim*3, n_cout, 
-                    kernel_size=n_hw, stride=1, padding=0),
-                nn.Sigmoid())
+                    kernel_size=n_hw, stride=1, padding=0),)
     return model
