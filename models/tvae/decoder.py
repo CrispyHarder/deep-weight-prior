changed logprob loss to recon loss, to be comparable to other models. Aldo should fix the DB161121.14 issue

models/tvae/decoder.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F
 import torch.nn as nn
 from torch.distributions.bernoulli import Bernoulli
 from torch.distributions.normal import Normal
@@ -28,16 +29,15 @@ class Bernoulli_Decoder(Decoder):
         return probs_x
 
 class Gaussian_Decoder(Decoder):
-    def __init__(self, model, scale=0.003):
+    def __init__(self, model, scale=1.0):
         super(Gaussian_Decoder, self).__init__(model)
         self.scale = torch.tensor([scale])
 
     def forward(self, z, x):
-        mu_x = self.model(z)
-        p = Normal(loc=mu_x, scale=self.scale.to(x.device))
-        neg_logpx_z = -1 * p.log_prob(x)
+        x_recon = self.model(z)
+        recon_loss = F.mse(x,x_recon)
 
-        return mu_x, neg_logpx_z
+        return x_recon, recon_loss
 
     def only_decode(self, z):
         mu_x = self.model(z)

models/tvae/tvae.py
@@ -18,9 +18,9 @@ class TVAE(torch.nn.Module):
         z, kl_z, _, _ = self.z_encoder(x)
         u, kl_u, _, _ = self.u_encoder(x)
         s = self.grouper(z, u)
-        probs_x, neg_logpx_z = self.decoder(s, x)
+        x_recon, recon_loss = self.decoder(s, x)
 
-        return z, u, s, probs_x, kl_z, kl_u, neg_logpx_z
+        return z, u, s, x_recon, kl_z, kl_u, recon_loss
 
     def get_IS_estimate(self, x, n_samples=100):
         log_likelihoods = []