0b9ae2364748092c6dd5affeeb1688ff8dcf3695
CrispyHarder <christian.harder96@gmail.com>
2021-11-16 13:43:34 +0100

    deleted everything with a wandb dependency

--- models/tvae/decoder.py
+++ models/tvae/decoder.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 from torch.distributions.bernoulli import Bernoulli
 from torch.distributions.normal import Normal
-from models.tvae.utils import plot_filters
 
 class Decoder(nn.Module):
     def __init__(self, model):
@@ -11,12 +10,6 @@ class Decoder(nn.Module):
 
     def forward(self, x):
         raise NotImplementedError
-
-    def plot_weights(self, name, wandb_on=True):
-        name_w_idx = name + '_L{}'
-        for idx, layer in enumerate(self.model):
-            if isinstance(layer, nn.Conv2d) or isinstance(layer, nn.ConvTranspose2d):
-                plot_filters(layer.weight, name_w_idx.format(idx), wandb_on=wandb_on)
 
 
 class Bernoulli_Decoder(Decoder):
--- models/tvae/encoder.py
+++ models/tvae/encoder.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 from torch.distributions.kl import kl_divergence
 from torch.distributions.normal import Normal
-from models.tvae.utils import plot_filters
 
 class Encoder(nn.Module):
     def __init__(self, model, prior, n_params=1):
@@ -13,18 +12,6 @@ class Encoder(nn.Module):
 
     def forward(self, x):
         raise NotImplementedError
-
-    def plot_weights(self, name, wandb_on=True):
-        name_w_idx = name + '_L{}'
-        for idx, layer in enumerate(self.model):
-            if isinstance(layer, nn.Conv2d):
-                if idx == len(self.model) - 1:
-                    s_dim = layer.weight.shape[0] // self.n_params
-                    w = layer.weight.view(self.n_params, s_dim, *layer.weight.shape[1:])
-                    for i in range(self.n_params):
-                        plot_filters(w[i], name_w_idx.format(idx) + f'_p{i}', wandb_on=wandb_on)
-                else:                   
-                    plot_filters(layer.weight, name_w_idx.format(idx), wandb_on=wandb_on)
 
     def sample(self, x, n_samples, cap_dim):
         return self.prior.sample(torch.Size([n_samples, cap_dim**2, 1, 1])).to(x.device)
