08c644ac06f9172d148a7aa038e1c5e34ac2fc98
CrispyHarder <christian.harder96@gmail.com>
2022-02-09 19:35:31 +0100

    changed default values for training of resnet cifar

--- train-net-cifar.py
+++ train-net-cifar.py
@@ -61,7 +61,7 @@ parser = myexman.ExParser(file=__file__)
 parser.add_argument('--name', default='')
 parser.add_argument('--gpu_id', default='0')
 parser.add_argument('--seed', default=5743, type=int)
-parser.add_argument('--epochs', default=120, type=int, help='Number of epochs')
+parser.add_argument('--epochs', default=60, type=int, help='Number of epochs')
 parser.add_argument('--bs', default=128, type=int, help='Batch size')
 parser.add_argument('--test_bs', default=500, type=int, help='Batch size for test dataloader')
 
@@ -76,8 +76,8 @@ parser.add_argument('--mult_init_prior', type=str, default='',
 parser.add_argument('--lr', default=0.1, type=float, help='Initial learning rate')
 parser.add_argument('--weight_decay', default=1e-4, type=float)
 parser.add_argument('--momentum', default=0.9, type=float)
-parser.add_argument('--milestones', type=int, nargs='*', default=[40,50])
-parser.add_argument('--gammas', default=[0.5,0.5], nargs='*', type=float)
+parser.add_argument('--milestones', type=int, nargs='*', default=[80,100])
+parser.add_argument('--gammas', default=[0.5,0.2], nargs='*', type=float)
 
 
 args = parser.parse_args()
