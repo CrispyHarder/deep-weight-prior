e7111e3e472bcb22b6ff8557c3812635b6c46ac3
CrispyHarder <christian.harder96@gmail.com>
2022-02-14 14:33:47 +0100

    changed default values for vae training

--- train_multiple_vaeV2.py
+++ train_multiple_vaeV2.py
@@ -15,14 +15,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', type=str)
 
 parser.add_argument('--data_dir',default='') # for later use 
-parser.add_argument('--num_epochs', default=1000, type=int)
+parser.add_argument('--num_epochs', default=100, type=int)
 parser.add_argument('--batch_size', default=128, type=int)
 parser.add_argument('--seed', default=42, type=int)
 parser.add_argument('--gpu_id', default='0')
 
 #optimisation 
 parser.add_argument('--lr', default=0.001, type=float)
-parser.add_argument('--weight_decay', type=float, default=0.)
+parser.add_argument('--weight_decay', type=float, default=0.0001)
 
 #evaluation
 parser.add_argument('--test_bs', default=512, type=int)
