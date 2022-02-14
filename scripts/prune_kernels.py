5e0e4d68500893c1f36e4d6e97677abcb9b60c98
CrispyHarder <christian.harder96@gmail.com>
2022-02-14 11:34:34 +0100

    adapted kernel pruning to new data directory format

--- scripts/prune_kernels.py
+++ scripts/prune_kernels.py
@@ -3,24 +3,17 @@ import numpy as np
 import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-root', type=str, default='data',
-                    help='where to unload the saved kernel slices ')
-parser.add_argument('-arch', type=str, default='resnet20',
-                    help='where to unload the saved kernel slices ')
-parser.add_argument('-D', type=str, default='3',
-                    help='where to unload the saved kernel slices ')
+parser.add_argument('-dataset', type=str)
 parser.add_argument('-percent', type=int, default=5,
                     help='slices with lowest x percent are pruned away')
-
+                    
 args = parser.parse_args()
 
-k_dim = args.D 
-dim_str = str(k_dim)+'x'+str(k_dim)
-path_to_convs = os.path.join(args.root,args.arch,dim_str)
+source_dataset = os.path.join('data',f'resnet20_{args.dataset}','3x3')
 
-for layer in os.listdir(path_to_convs):
+for layer in os.listdir(source_dataset):
     for split in ['train','test']:
-        p_conv = os.path.join(path_to_convs,layer,'conv',split+'.npy')
+        p_conv = os.path.join(source_dataset,layer,'conv',split+'.npy')
         conv = np.load(p_conv)
         conv_norms = np.array([np.linalg.norm(slice) for slice in conv])
         threshold = np.percentile(conv_norms,args.percent)
